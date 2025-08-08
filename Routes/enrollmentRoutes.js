b09ded8ac89d33310dfe0eefc53e0d34aecc3da3
Patrickodida <patrick.o.odida@gmail.com>
2025-08-08 17:15:57 +0300

    Add relevant comments to the routes

--- Routes/enrollmentRoutes.js
+++ Routes/enrollmentRoutes.js
@@ -5,10 +5,15 @@ const validateToken = require('../Middleware/validateToken')
 
 // Enroll User (Protected)
 router.post('/', validateToken, enrollUser);
+// Retrieve all students enrolled
 router.get('/course/:courseId', validateToken, getCourseEnrollments);
+// Retrieve user enrollment
 router.get('/:userId', validateToken, getUserEnrollments);
+// Retrieve a single user enrollment
 router.get('/:userId/:courseId', validateToken, getEnrollment);
+// Update/mark lesson as complete
 router.put('/:userId/:courseId/complete', validateToken, markLessonCompleted);
+// Delete a user from a course
 router.delete('/:userId/:courseId', validateToken, unenrollUser);
 
 module.exports = router;
