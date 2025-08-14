3f1f0ee00341d225093031702242cf4be4625fef
Patrickodida <patrick.o.odida@gmail.com>
2025-08-14 22:04:22 +0300

    Add role based checks and authentication on the course routes.

--- Routes/courseRoutes.js
+++ Routes/courseRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { createCourse, getAllCourses, updateCourse, deleteCourse, getCourseById, toggleCoursePublish } = require('../Controllers/courseController')
 const validateToken = require('../Middleware/validateToken')
+const { isAuthenticated, hasRole } = require('../Middleware/auth');
 
 router.get('/', getAllCourses);
 router.get('/:id', getCourseById);
-router.post('/', validateToken, createCourse);
-router.put('/:id', validateToken, updateCourse);
-router.delete('/:id', validateToken, deleteCourse);
-router.patch('/:id/publish', validateToken, toggleCoursePublish);
+router.post('/', validateToken,isAuthenticated, hasRole('INSTRUCTOR', 'ADMIN'), createCourse);
+router.put('/:id', validateToken, isAuthenticated, hasRole('INSTRUCTOR', 'ADMIN'), updateCourse);
+router.delete('/:id', validateToken, isAuthenticated, hasRole('INSTRUCTOR', 'ADMIN'), deleteCourse);
+router.patch('/:id/publish', validateToken, isAuthenticated, hasRole('INSTRUCTOR', 'ADMIN'), toggleCoursePublish);
 
 module.exports = router;
